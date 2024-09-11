d174a8dec858f16f744c8d05789d96270d27c8ea
Jeremy Grosser <jeremy@synack.me>
2024-09-11 11:19:46 -0700

    platform indepdendence

--- src/s-macres.adb
+++ src/s-macres.adb
@@ -29,77 +29,13 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-with Interfaces;
-with System.Machine_Code; use System.Machine_Code;
-
 package body System.Machine_Reset is
-
-   procedure Os_Exit (Status : Integer);
-   pragma No_Return (Os_Exit);
-   pragma Export (Ada, Os_Exit, "_exit");
-   --  Shutdown or restart the board
-
-   procedure Os_Abort;
-   pragma No_Return (Os_Abort);
-   pragma Export (Ada, Os_Abort, "abort");
-   --  Likewise
-
-   --------------
-   -- Os_Abort --
-   --------------
-
-   procedure Os_Abort is
-   begin
-      Os_Exit (1);
-   end Os_Abort;
-
-   -------------
-   -- Os_Exit --
-   -------------
-
-   procedure Os_Exit (Status : Integer) is
-      pragma Unreferenced (Status);
-      --  The parameter is just for ISO-C compatibility
-
-      AIRCR : Interfaces.Unsigned_32 with
-        Address => 16#E000_ED0C#,
-        Import,
-        Volatile;
-
-   begin
-      --  Apply a barrier prior to the reset request to ensure previous
-      --  memory accesses complete before the reset occurs.
-
-      Asm ("dsb 0xF", Volatile => True, Clobber => "memory");
-
-      --  Depending on the implementation, the reset could take some time to
-      --  occur, during which an interrupt could come in. In that case the
-      --  reset could occur in the middle of the interrupt handler. Disable
-      --  interrupts to prevent that.
-
-      Asm ("cpsid i", Volatile => True);
-
-      --  Request reset
-
-      AIRCR := 16#05FA_0004#;
-
-      --  Depending on the implementation, the processor could continue
-      --  to execute a few instructions following the reset request before
-      --  the reset actually takes place. Use a loop to ensure there is no
-      --  application code to execute following the request.
-
-      loop
-         null;
-      end loop;
-   end Os_Exit;
-
-   ----------
-   -- Stop --
-   ----------
 
    procedure Stop is
    begin
-      Os_Exit (0);
+      loop
+         null;
+      end loop;
    end Stop;
 
 end System.Machine_Reset;
